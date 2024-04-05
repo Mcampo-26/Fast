5185001812b060bf44c7b0f9b36f8bd474ed750a
Mauricio Campo <mcampo26@gmail.com>
2024-04-05 11:12:34 -0300

    cambio ruta de imagenes

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -29,16 +29,16 @@ export const Carrusel = () => {
 <div className="max-w-screen-xl mx-auto py-8">
   <Slider {...settings}>
     <div>
-      <img src='src\assets\img\fast1.png' alt="Image 1" className="w-full max-h-48 md:max-h-80 object-contain" />
+      <img src='https://gtsinformatica.com/wp-content/uploads/2020/10/reparacion-de-notebook-1.jpg' alt="Image 1" className="w-full max-h-48 md:max-h-80 object-contain" />
     </div>
     <div>
       <a href="https://www.instagram.com/reel/C2TYR1GOtGB/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" target="_blank" rel="noopener noreferrer">
-        <img src="src\assets\img\fast3.png" alt="Image 2" className="w-full max-h-48 md:max-h-80 object-contain" />
+        <img src="https://img.freepik.com/fotos-premium/reparador-computadoras-arreglando-nuevo-componente-laptop-computadora-reparacion-concepto_106035-1489.jpg" alt="Image 2" className="w-full max-h-48 md:max-h-80 object-contain" />
       </a>
     </div>
     <div>
       <a href="https://www.instagram.com/reel/C2TYR1GOtGB/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" target="_blank" rel="noopener noreferrer">
-        <img src="src\assets\img\fast2.png" alt="Image 3" className="w-full max-h-48 md:max-h-80 object-contain" />
+        <img src="https://i0.wp.com/ensedeciencia.com/wp-content/uploads/2023/04/Copia-de-C-EdCiencia-P-2023-04-23T184139.080.jpg?resize=800%2C451&ssl=1" alt="Image 3" className="w-full max-h-48 md:max-h-80 object-contain" />
       </a>
     </div>
   </Slider>

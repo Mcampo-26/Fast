3b810bec83cc1404154d21a0e099a094cfe65fe3
Mauricio Campo <mcampo26@gmail.com>
2024-04-19 10:06:10 -0300

    agrego carte de bar amercano

--- src/components/Carta.jsx
+++ src/components/Carta.jsx
@@ -4,30 +4,47 @@ import './Css/Carta.css';
 export const Carta = () => {
   return (
     <>
-      <header>
-        <img src="https://restoamericano.vercel.app/assets/logo2-392bb098.png" alt="Logo" className="h-20 mx-auto margen" />
+    <div className='bg-carta'>
+      <header  >
+        <img src="https://restoamericano.vercel.app/assets/logo2-392bb098.png" alt="Logo" className="h-20 mx-auto margen " />
       </header>
+      
       <nav className="w-1/4 mx-auto mt-8">
         <div className="p-4 margen">
           <ul className="space-y-2">
-            <li>
-              <a href="#" className="block text-center text-white text-lg md:text-xl  lg:text-2xl hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 md:text-base lg:text-lg">CAFETERÍA</a>
-            </li>
-            <li>
-              <a href="#" className="block text-center text-white text-lg md:text-xl lg:text-2xl hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 md:text-base lg:text-lg">PLATO PRINCIPAL</a>
-            </li>
-            <li>
-              <a href="#" className="block text-center text-white text-lg md:text-xl lg:text-2xl hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 md:text-base lg:text-lg">BEBIDAS SIN ALCOHOL</a>
-            </li>
-            <li>
-              <a href="#" className="block text-center text-white text-lg md:text-xl lg:text-2xl hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 md:text-base lg:text-lg">BEBIDAS CON ALCOHOL</a>
-            </li>
-            <li>
-              <a href="#" className="block text-center text-white text-lg md:text-xl lg:text-2xl hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 md:text-base lg:text-lg">MENÚ COMPLETO</a>
-            </li>
+          <li>
+  <a href="#" className="block text-center text-white text-lg  hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 ">
+    CAFETERÍA
+  </a>
+</li>
+<li>
+  <a href="https://drive.google.com/file/d/14O64_u_VmgnNAU9LLI1jgzB9Y9xPwibn/view?usp=sharing" target="_blank" className="block text-center text-white text-lg  hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 ">
+    PLATO PRINCIPAL
+  </a>
+</li>
+<li>
+  <a href="#" className="block text-center text-white text-lg hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 
+  ">
+    BEBIDAS SIN ALCOHOL
+  </a>
+</li>
+<li>
+  <a href="#" className="block text-center text-white text-lg hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 
+  ">
+    BEBIDAS CON ALCOHOL
+  </a>
+</li>
+<li>
+  <a href="#" className="block text-center text-white text-lg hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 
+  ">
+    MENÚ COMPLETO
+  </a>
+</li>
+
           </ul>
         </div>
       </nav>
+      </div>
     </>
   );
 };

0299f8233d504fba4489b56d0522279147bc96d0
Mauricio Campo <mcampo26@gmail.com>
2024-04-19 11:34:45 -0300

    cambio direccion de pdf

--- src/components/Carta.jsx
+++ src/components/Carta.jsx
@@ -18,7 +18,7 @@ export const Carta = () => {
   </a>
 </li>
 <li>
-  <a href="https://www.dropbox.com/scl/fi/g39vffw00o1oq6a5hq44n/MUESTRA-MENU-AMERICANO2023.pdf?rlkey=2o4iz3vdsb3msc8fsjf69tt7a&st=b5hdq3r7&dl=0" target="_blank" className="block text-center text-white text-lg  hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 ">
+  <a href="https://raw.githubusercontent.com/Mcampo-26/Fast/2f9e430e489319c68021388e8b7649e31ed553fc/MUESTRA-MENU-AMERICANO2023.pdf" target="_blank" className="block text-center text-white text-lg  hover:text-black hover:bg-white px-4 py-2 rounded-lg border bg-olive transition-all duration-300 ">
     PLATO PRINCIPAL
   </a>
 </li>
